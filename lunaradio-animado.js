afef1d1c3c348c7e60694350d7ae4dbadbf29b33
OlgaAmore <156131929+OlgaAmore@users.noreply.github.com>
2024-07-05 13:12:56 +0200

    Update lunaradio-animado.js

--- lunaradio-animado.js
+++ lunaradio-animado.js
@@ -474,7 +474,7 @@ y("", "")
 
 
 function zeno() {
-zurl = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/"+dzsuinmlh3mtv+"/?rand="+ Math.random();
+zurl = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/"+6bdbnqwnsg8uv+"/?rand="+ Math.random();
 if (sa === "fallback") {url = "fallback.php?url="+zurl} else {url = zurl}
 
 d.ajax({
